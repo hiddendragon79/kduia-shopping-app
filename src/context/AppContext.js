05a75ba9849f647d94879a7da2a8a2c24536cec5
Subroto Sikder <hidden.dragon79@gmail.com>
2024-07-15 13:02:44 +0600

    first commit from local machine

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,16 @@
 import React, { createContext, useReducer } from 'react';
+
+export const AppReducer=(state,action)=>{
+    let new_expenses=[];
+
+    switch(action.type){
+        case 'ADD_QUANTITY':
+            let updatedQty=false;
+            state.expenses.map((expense)=>{
+                if(expense.name===action.payload.name){
+                    expense.quantity=expense.quantity+action.payload.quantity;
+                    updatedQty=true;
+                }
+            })
+    };
+}
